a37e48808811c6a96984c3e94825a8bef0caaf20
Luis G. Villegas <luisguveal@gmail.com>
2022-03-13 18:51:49 -0430

    Setting up database on production

--- config/database.js
+++ config/database.js
@@ -1,11 +1,39 @@
 const path = require('path');
+const parse = require('pg-connection-string').parse;
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  if(env('NODE_ENV') === 'production'){
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          },
+          options: {
+            ssl: true,
+          },
+        },
+      },
+    }
+  }
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  }
+};
