0ea574a8000e0fd66a650da559cdbee9f8c090b0
Luis G. Villegas <luisguveal@gmail.com>
2022-03-19 11:16:00 -0430

    Configuring middleware for S3

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,13 +5,6 @@ const url = `${bucket}.s3.${region}.amazonaws.com`
 
 module.exports = [
   'strapi::errors',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::favicon',
-  'strapi::public',
   {
     name: 'strapi::security',
     config: {
@@ -26,4 +19,11 @@ module.exports = [
       },
     },
   },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::favicon',
+  'strapi::public',
 ];
