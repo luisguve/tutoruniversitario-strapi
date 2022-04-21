55e7e52dc5307fdd91329391eb89aa0e1db44eea
Luis G. Villegas <luisguveal@gmail.com>
2022-04-20 21:35:28 -0430

    Migrating to v2.1.8

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '0913985501596d3c0d861d51675eec14'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
 });
